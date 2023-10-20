719761c19a4cc73fa036596582c5d04cd4fd8ca7
YoyoMy <ioana.miteva@hotmail.com>
2023-10-20 16:46:12 +0200

    code clean up

--- lib/drivers/Wifi/Wifi_driver.c
+++ lib/drivers/Wifi/Wifi_driver.c
@@ -14,6 +14,7 @@ static const char *HTML_FORM = "<html><form action=\"/\" method=\"post\">"
 static int retry_cnt = 0;
 EventGroupHandle_t wifi_events;
 
+// AP Mode
 void init_wifi_ap()
 {
     if (nvs_flash_init() != ESP_OK)
@@ -44,106 +45,6 @@ void init_wifi_ap()
     esp_wifi_start();
 
     start_webserver();
-}
-void init_wifi_sta(char ssid[20], char pass[20])
-{
-    if (nvs_flash_init() != ESP_OK)
-    {
-        nvs_flash_erase();
-        nvs_flash_init();
-    }
-    wifi_events = xEventGroupCreate();
-    esp_event_loop_create_default();
-    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &handle_wifi_connection, NULL);
-    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &handle_wifi_connection, NULL);
-
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = WIFI_SSID,
-            .password = WIFI_PASS,
-            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-            .pmf_cfg = {
-                .capable = true,
-                .required = false},
-        },
-    };
-    esp_netif_init();
-    esp_netif_create_default_wifi_sta();
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&cfg);
-    esp_wifi_set_mode(WIFI_MODE_STA);
-    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config);
-    esp_wifi_start();
-    ESP_LOGI(TAG, "Connected to wifi: %s", ssid);
-}
-void init_wifi(int mode)
-{
-    if (nvs_flash_init() != ESP_OK)
-    {
-        nvs_flash_erase();
-        nvs_flash_init();
-    }
-
-    if (mode == STA)
-    {
-        wifi_events = xEventGroupCreate();
-        esp_event_loop_create_default();
-        esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &handle_wifi_connection, NULL);
-        esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &handle_wifi_connection, NULL);
-
-        // setenv(WIFI_PASS, "value", 1);
-        /* wifi_config_t wifi_config = {
-             .sta = {
-                 .ssid = WIFI_SSID,
-                 .password = WIFI_PASS,
-                 .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-                 .pmf_cfg = {
-                     .capable = true,
-                     .required = false},
-             },
-         };
-         esp_netif_init();
-         esp_netif_create_default_wifi_sta();
-         wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-         esp_wifi_init(&cfg);
-         esp_wifi_set_mode(WIFI_MODE_STA);
-         esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config);
-         esp_wifi_start();*/
-    }
-    else
-    {
-        esp_event_loop_create_default();
-        esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &handle_wifi_events, NULL);
-
-        esp_netif_init();
-        esp_netif_create_default_wifi_ap();
-        wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-        esp_wifi_init(&cfg);
-
-        wifi_config_t wifi_config = {
-            .ap = {
-                .ssid = WIFI_SSID_AP,
-                .ssid_len = strlen(WIFI_SSID_AP),
-                .channel = WIFI_CHANNEL,
-                .password = WIFI_PWD,
-                .max_connection = MAX_CONN_CNT,
-                .authmode = WIFI_AUTH_WPA_WPA2_PSK},
-        };
-        esp_wifi_set_mode(WIFI_MODE_AP);
-        esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config);
-        esp_wifi_start();
-
-        start_webserver();
-    }
-}
-
-void handle_wifi_events(void *arg, esp_event_base_t event_base,
-                        int32_t event_id, void *event_data)
-{
-    if (event_id == WIFI_EVENT_AP_STACONNECTED)
-    {
-        ESP_LOGI(TAG, "a station connected");
-    }
 }
 
 void start_webserver(void)
@@ -186,7 +87,7 @@ esp_err_t handle_http_post(httpd_req_t *req)
     int index = (int)(andIndex - content);
     char ssid[20], pass[20];
     strncpy(ssid, content + 5, index - 5);
-    strcpy(pass, content + index + 5);
+    strncpy(pass, content + index + 5, strlen(content) - index);
     for (int i = 0; i < strlen(ssid); i++)
     {
         if (ssid[i] == '+')
@@ -203,7 +104,49 @@ esp_err_t handle_http_post(httpd_req_t *req)
 
     ESP_LOGI(TAG, "%s, %s", WIFI_SSID, WIFI_PASS);
     init_wifi_sta(ssid, pass);
-    // return httpd_resp_send(req, "received", HTTPD_RESP_USE_STRLEN);
+    return httpd_resp_send(req, "received", HTTPD_RESP_USE_STRLEN);
+}
+
+// STA Mode
+void init_wifi_sta(char ssid[20], char pass[20])
+{
+    if (nvs_flash_init() != ESP_OK)
+    {
+        nvs_flash_erase();
+        nvs_flash_init();
+    }
+    wifi_events = xEventGroupCreate();
+    esp_event_loop_create_default();
+    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &handle_wifi_connection, NULL);
+    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &handle_wifi_connection, NULL);
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASS,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+            .pmf_cfg = {
+                .capable = true,
+                .required = false},
+        },
+    };
+    esp_netif_init();
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);
+    esp_wifi_set_mode(WIFI_MODE_STA);
+    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config);
+    esp_wifi_start();
+    ESP_LOGI(TAG, "Connected to wifi: %s", ssid);
+}
+
+void handle_wifi_events(void *arg, esp_event_base_t event_base,
+                        int32_t event_id, void *event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED)
+    {
+        ESP_LOGI(TAG, "a station connected");
+    }
 }
 
 void handle_wifi_connection(void *arg, esp_event_base_t event_base,
